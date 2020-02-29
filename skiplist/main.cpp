ba72515b04fc5512a3a824a4f74d3dd504bd30d7
Tri <1203732857@qq.com>
2020-02-29 23:41:56 +0800

    transplant the skiplist of redis

---
+++ skiplist/main.cpp
@@ -0,0 +1,16 @@
+/*
+@Author: TriAlley
+@Date: 2020/02/25
+@refrence:
+	1. Skip Lists : A Probabilistic Alternative to Balanced Trees
+	2. https://www.cnblogs.com/gqtcgq/p/7247074.html
+*/
+#include "skiplist.h"
+
+
+int main () {
+
+
+
+	return 0;
+}
---
+++ skiplist/skiplist.h
@@ -0,0 +1,10 @@
+/*
+@Author: TriAlley
+@Date: 2020/02/25
+@refrence:
+	1. Skip Lists : A Probabilistic Alternative to Balanced Trees
+	2. https://www.cnblogs.com/gqtcgq/p/7247074.html
+*/
+#pragma once
+
+template<>
