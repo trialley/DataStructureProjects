277ef41f58206339a0515e1d56a148a165bfb5a6
Tri <1203732857@qq.com>
2020-03-01 14:54:40 +0800

    oj done

--- skiplistOJ/main.cpp
+++ skiplistOJ/main.cpp
@@ -466,3 +466,159 @@ public:
         return nullptr;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+//
+///*****************************************************************************
+// * 函 数 名  : skiplistDelete
+// * 函数功能  : 根据给定分值和成员来删除节点
+// * 输入参数  : skiplist *skiplist  表头指针
+//               double score    节点分数
+//               robj *data       节点数据指针
+// * 输出参数  : 无
+// * 返 回 值  :
+// * 调用关系  :
+// * 记    录
+// * 1.日    期: 2017年06月13日
+// *   作    者: zyz
+// *   修改内容: 新生成函数
+//*****************************************************************************/
+//long long skiplistDelete (skiplist* skiplist, double score, robj* data) {
+//    skiplistNode<K,E>* update[skipLIST_MAXLEVEL], * x;
+//    long long i;
+//
+//    x = skiplist->_header;
+//    // 遍历所有层，记录删除节点后需要被修改的节点到 update 数组  
+//    for (i = skiplist->_level - 1; i >= 0; i--) {
+//        //指针前移首要条件是前向节点指针不为空，次要条件是分数小于指定分数，或即使分数相等，节点成员对象也不相等------> 前向指针前移的必要条件：分数小于或等于指定分数
+//        while (x->level[i].forward && //前向指针不为空
+//            (x->level[i].forward->score < score || //前向节点分数小于指定分数
+//            (x->level[i].forward->score == score &&  //前向节点分数等于指定分数
+//                compareObjects (x->level[i].forward->data, data) < 0)))//前向节点成员对象不相同
+//            x = x->level[i].forward;
+//        //保存待删除节点的前一节点指针
+//        update[i] = x;
+//    }
+//    // 因为多个不同的 member 可能有相同的 score  
+//    // 所以要确保 x 的 member 和 score 都匹配时，才进行删除  
+//
+//    x = x->level[0].forward;
+//
+//    if (x && score == x->score && equalObjects (x->data, data)) {
+//        skiplistDeleteNode (skiplist, x, update);
+//        free (x);
+//        return 1;
+//    }
+//    return 0; /* not found */
+//}
+
+
+#include<string>
+
+#include<iostream>
+using namespace std;
+
+/*
+apple
+-1
+5 oops
+1 mi
+10 apple
+*/
+
+/*输入包含 1+n+m 行数据
+第一行包含两个数字 n, m (0 \leq n \leq 1000, 0 \leq m \leq 10000≤n≤1000,0≤m≤1000)
+之后的 n 行，每行包含一个数字与一个字符串，表示一个元素，数字为跳表的关键字(0\leq key \leq 1,000,000,000)(0≤key≤1,000,000,000)，字符串为元素的值。
+之后的 m 行表示 m 个跳表ADT操作。每行第一个数字 a 用来区别操作。
+
+a 为 1 时，表示查找操作，其后的一个数字 b 为要查询的元素的关键字， 输出 该元素的值，不存在则输出 -1。
+a 为 2 时，表示插入操作，其后的一个数字 b 为要插入的元素，一个字符串 c 为对应的值， 不输出 。
+a 为 3 时，表示删除操作，其后的一个数字 b 为要删除的关键字， 输出 该关键字与其对应的元素值，若删除关键字不存在或删除失败，输出 -1。
+a 为 4 时，表示删除最小元素操作。 输出 跳表内最小关键字 c 与其对应的元素值，并将对应元素在跳表内部删除
+a 为 5 时，表示删除最大元素操作。 输出 跳表内最大关键字 c 与其对应的元素值，并将对应元素在跳表内部删除*/
+int main () {
+#pragma warning(disable:4996)
+  // freopen ("in.txt", "r", stdin);
+
+    skiplist<long long, string> z;
+
+    long long n, m;
+    cin >> n >> m;
+
+    for (long long i = 0; i < n; i++) {
+        long long  a;
+        cin >> a;
+        string b;
+        cin >> b;
+        z.insert (a, b);
+
+    }
+
+
+    for (long long i = 0; i < m; i++) {
+        long long a;
+        cin >> a;
+        if (a == 1) {
+            long long b;
+            cin >> b;
+            string* p = z.skiplistGetDataByScore (b);
+            if (p) {
+                cout << *p;
+            } else {
+                cout << "-1";
+            }
+            cout << "\n";
+        } else if (a == 2) {
+            long long b;
+            cin >> b;
+            string c;
+            cin >> c;
+            z.insert (b, c);
+        } else if (a == 3) {
+            long long b;
+            cin >> b;
+            string* p = z.skiplistGetDataByScore (b);
+            if (p) {
+                cout << b << " " << *p;
+                z.skiplistDeleteByScore (b);
+            } else {
+                cout << "-1";
+            }
+            cout << "\n";
+        } else if (a == 4) {
+            string* e = z.getMin ();
+            long long* k = z.getMinScore ();
+            if (e) {
+                cout << *k << " " << *e;
+            } else {
+                cout << "-1";
+            }
+            z.skiplistDeleteByScore (*k);
+
+            cout << "\n";
+        } else if (a == 5) {
+            string* e = z.getMax ();
+            long long* k = z.getMaxScore ();
+
+            if (e) {
+                cout << *k << " " << *e;
+            } else {
+                cout << "-1";
+            }
+            z.skiplistDeleteByScore (*k);
+            cout << "\n";
+        }
+    }
+
+
+}
--- skiplistRedisOO/main.cpp
+++ skiplistRedisOO/main.cpp
@@ -1,15 +1,101 @@
+
+
+#include<string>
 #include"skiplist.h"
-
-
 #include<iostream>
 using namespace std;
 
+/*
+apple
+-1
+5 oops
+1 mi
+10 apple
+*/
 
+/*输入包含 1+n+m 行数据
+第一行包含两个数字 n, m (0 \leq n \leq 1000, 0 \leq m \leq 10000≤n≤1000,0≤m≤1000)
+之后的 n 行，每行包含一个数字与一个字符串，表示一个元素，数字为跳表的关键字(0\leq key \leq 1,000,000,000)(0≤key≤1,000,000,000)，字符串为元素的值。
+之后的 m 行表示 m 个跳表ADT操作。每行第一个数字 a 用来区别操作。
+
+a 为 1 时，表示查找操作，其后的一个数字 b 为要查询的元素的关键字， 输出 该元素的值，不存在则输出 -1。
+a 为 2 时，表示插入操作，其后的一个数字 b 为要插入的元素，一个字符串 c 为对应的值， 不输出 。
+a 为 3 时，表示删除操作，其后的一个数字 b 为要删除的关键字， 输出 该关键字与其对应的元素值，若删除关键字不存在或删除失败，输出 -1。
+a 为 4 时，表示删除最小元素操作。 输出 跳表内最小关键字 c 与其对应的元素值，并将对应元素在跳表内部删除
+a 为 5 时，表示删除最大元素操作。 输出 跳表内最大关键字 c 与其对应的元素值，并将对应元素在跳表内部删除*/
 int main () {
-	skiplist sl;
-	sl.skiplistInsert (1, 0);
-	sl.skiplistInsert (2, 0);
-	sl.skiplistInsert (3, 0);
-	sl.skiplistDeleteByScore (2);
-	return 0;
+#pragma warning(disable:4996)
+    freopen ("in.txt", "r", stdin);
+
+    skiplist<long long, string> z;
+
+    long long n, m;
+    cin >> n >> m;
+
+    for (long long i = 0; i < n; i++) {
+        long long  a;
+        cin >> a;
+        string b;
+        cin >> b;
+        z.insert (a, b);
+
+    }
+
+
+    for (long long i = 0; i < m; i++) {
+        long long a;
+        cin >> a;
+        if (a == 1) {
+            long long b;
+            cin >> b;
+            string* p = z.skiplistGetDataByScore (b);
+            if (p) {
+                cout << *p;
+            } else {
+                cout << "-1";
+            }
+            cout << "\n";
+        } else if (a == 2) {
+            long long b;
+            cin >> b;
+            string c;
+            cin >> c;
+            z.insert (b, c);
+        } else if (a == 3) {
+            long long b;
+            cin >> b;
+            string* p = z.skiplistGetDataByScore (b);
+            if (p) {
+                cout << b << " " << *p;
+                z.skiplistDeleteByScore (b);
+            } else {
+                cout << "-1";
+            }
+            cout << "\n";
+        } else if (a == 4) {
+            string* e = z.getMin ();
+            long long* k = z.getMinScore ();
+            if (e) {
+                cout << *k << " " << *e;
+            } else {
+                cout << "-1";
+            }
+            z.skiplistDeleteByScore (*k);
+
+            cout << "\n";
+        } else if (a == 5) {
+            string* e = z.getMax ();
+            long long* k = z.getMaxScore ();
+
+            if (e) {
+                cout << *k << " " << *e;
+            } else {
+                cout << "-1";
+            }
+            z.skiplistDeleteByScore (*k);
+            cout << "\n";
+        }
+    }
+
+
 }
